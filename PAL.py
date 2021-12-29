157eca7d3568e511429be00fe0fa2497fb220911
Alex Netsch <alex.netsch97@gmail.com>
2021-12-29 22:51:13 +0100

    Remove screen bottom, various changes to accomidate 144 LEDs (from 60)

--- PAL.py
+++ PAL.py
@@ -17,6 +17,17 @@ def rgb_running_mean(x, k):
     x_new[:, 2] = np.convolve(x[:, 2], k, mode="same")
     return x_new
 
+def cut_img_divisible(img, div):
+    """
+    Cuts an image to make it divisible by a number along axis 0.
+    :param img: The image to cut.
+    :param div: The number to cut the image by.
+    :return: The cut image. 
+    """
+    s_div = (img.shape[0] % div)//2
+    img = img[s_div:-s_div, :]
+    return img
+
 #def calc_rgb_vals(screencap, plot_test_img=False):
 def calc_rgb_vals(screencap, plot_test_img=False):
     """
@@ -31,30 +42,32 @@ def calc_rgb_vals(screencap, plot_test_img=False):
     # use color temp of 6500K
     ctemp_6500k = np.array([255, 249, 253])
     ctemp_3500k = np.array([255, 196, 137])
-    ctemp_calib = np.array([200, 255, 100]) # calibration so white is white, different for your LEDs
+    ctemp_calib = np.array([255, 230, 100]) # calibration so white is white, different for your LEDs
     ctemp_scale = ctemp_calib / np.array([255, 255, 255])
     # scale brightness, otherwise LEDs are too bright
     ctemp_scale *= 0.2
 
-    border_width = 200
-    n_leds_short = 18
-    n_leds_long = 32
+    border_width = 300
+    n_leds_short = 35
+    n_leds_long = 72
 
     kernel_long = np.ones(20) / 20
     kernel_short = np.ones(10) / 10
-    # avg the screen borders
+    # avg and smooth the screen borders
     avg_top = np.mean(screencap[:border_width, :], axis=0)
     avg_top = rgb_running_mean(avg_top, kernel_long)
-    avg_bot = np.mean(screencap[-border_width:, :], axis=0)
-    avg_bot = rgb_running_mean(avg_bot, kernel_long)
     avg_left = np.mean(screencap[:, :border_width], axis=1)
     avg_left = rgb_running_mean(avg_left, kernel_short)
     avg_right = np.mean(screencap[:, -border_width:], axis=1)
     avg_right = rgb_running_mean(avg_right, kernel_short)
     
+    # make screen size divisible by number of leds
+    avg_top = cut_img_divisible(avg_top, n_leds_long)
+    avg_left = cut_img_divisible(avg_left, n_leds_short)
+    avg_right = cut_img_divisible(avg_right, n_leds_short)
+
     # avg the pixels for each LED
     vals_top = (avg_top.reshape((n_leds_long, -1, 3)).mean(axis=1) * ctemp_scale).astype(int)
-    vals_bot = (avg_bot.reshape((n_leds_long, -1, 3)).mean(axis=1) * ctemp_scale).astype(int)
     vals_left = (avg_left.reshape((n_leds_short, -1, 3)).mean(axis=1) * ctemp_scale).astype(int)
     vals_right = (avg_right.reshape((n_leds_short, -1, 3)).mean(axis=1) * ctemp_scale).astype(int)
 
@@ -62,7 +75,6 @@ def calc_rgb_vals(screencap, plot_test_img=False):
     if plot_test_img:
         img_test = np.zeros((n_leds_short, n_leds_long, 3), dtype=int)
         img_test[0, :,:] = vals_top
-        img_test[-1, :,:] = vals_bot
         img_test[:, 0, :] = vals_left
         img_test[:, -1, :] = vals_right
         plt.matshow(img_test)
@@ -74,21 +86,19 @@ def calc_rgb_vals(screencap, plot_test_img=False):
 def main():
     UDP_IP = "192.168.178.32"
     UDP_PORT = 4210
-
     print("UDP target IP: %s" % UDP_IP)
     print("UDP target port: %s" % UDP_PORT)
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
         d = d3dshot.create(capture_output="numpy")
         out = d.screenshot()
-        rgb_vals_old = calc_rgb_vals(out, plot_test_img=0)
         while 1:
             t0 = time.time()
             out = d.screenshot()
             rgb_vals = calc_rgb_vals(out, plot_test_img=0)
-            rgb_vals = (rgb_vals_old * 0.1 + rgb_vals * 0.9).astype(int)
             MESSAGE = bytes(list(rgb_vals.flatten()))
             sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
             print(f"Processing took {(time.time()-t0)}")
+            time.sleep(0.0166) # ~60 FPS, need to limit this when using 144 LEDs or ESP cant handle
 
 if __name__ == "__main__":
     main()
